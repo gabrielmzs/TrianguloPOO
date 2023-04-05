bd3c0720607fe4bf3c458ccf35cd5909561a3c28
Gabriel <mzsgabriel@gmail.com>
2023-04-05 17:29:23 -0300

    Add project files.

---
+++ TrianguloPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TrianguloPOO {
+    internal class Program {
+        static void Main(string[] args) {
+            Triangulo triangulo= new Triangulo();
+            Console.WriteLine("Informe o lado 1: ");
+            triangulo.lado1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o lado 2: ");
+            triangulo.lado2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o lado 3: ");
+            triangulo.lado3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(triangulo.VerificarLados());
+        }
+    }
+}
---
+++ TrianguloPOO/Triangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloPOO {
+    internal class Triangulo {
+
+        public double lado1, lado2, lado3;
+        public string mensagem;
+
+        public string VerificarLados() {
+
+            if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2) {
+
+                if (lado1 == lado2 && lado2 == lado3) {
+                    mensagem = "O triângulo é equilátero.";
+                } else 
+                    {
+                    if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+                        mensagem = "O triângulo é isóscele.";
+                    } else {
+                        mensagem = "O triângulo é escaleno.";
+                    }
+                }
+            } else {
+                mensagem = "Os comprimentos informados não formam um triângulo.";
+            }
+
+            return mensagem;
+        }
+
+
+    }
+}
+
